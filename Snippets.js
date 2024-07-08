aac44a358632fe87f693f3fe30d171075a747180
YodaNOW <vinod.fredrick@icloud.com>
2024-07-08 13:05:56 +0530

    Update Snippets.js

--- Snippets.js
+++ Snippets.js
@@ -1,1 +1,6 @@
 /** This is my JavaScript catch-all, its basically a lot of disjointed code snippets that are useful, some are simply fun */ 
+
+
+// If you ever need to - for some odd reason - force the Label of a field to align to the far left
+// Do the below in your client-scripts 
+g_form.getLabel('u_put_your_field_name').style="text-align: left;";
